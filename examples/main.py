ae900685ec21f7917a2d3a21fb095ad0560acf0b
Oniel <51058739+0niel@users.noreply.github.com>
2022-09-03 00:45:23 +0300

    Update main.py

--- examples/main.py
+++ examples/main.py
@@ -1,12 +1,12 @@
-import os
-
 from rtu_schedule_parser import ExcelScheduleParser
 from rtu_schedule_parser.constants import Degree, Institute
 from rtu_schedule_parser.downloader import ScheduleDownloader
 
 if __name__ == "__main__":
     downloader = ScheduleDownloader()
-    iit_docs = downloader.get_documents(specific_degrees={Degree.BACHELOR}, specific_institutes={Institute.IIT})
+    iit_docs = downloader.get_documents(
+        specific_degrees={Degree.BACHELOR}, specific_institutes={Institute.IIT}
+    )
     downloaded = downloader.download_all(iit_docs)
     print(downloaded)
 
