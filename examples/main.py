49c281970d0e804aa7f31afca9d7f3cd801a56d3
Oniel <51058739+0niel@users.noreply.github.com>
2022-09-03 01:33:02 +0300

    Update example

--- examples/main.py
+++ examples/main.py
@@ -1,15 +1,35 @@
-from rtu_schedule_parser import ExcelScheduleParser
+import os
+
+from rtu_schedule_parser import ExcelScheduleParser, ScheduleData
 from rtu_schedule_parser.constants import Degree, Institute
 from rtu_schedule_parser.downloader import ScheduleDownloader
 
 if __name__ == "__main__":
+    # Initialize downloader with default directory to save files
     downloader = ScheduleDownloader()
+    # Get documents for specified institute and degree
     iit_docs = downloader.get_documents(
         specific_degrees={Degree.BACHELOR}, specific_institutes={Institute.IIT}
     )
-    downloaded = downloader.download_all(iit_docs)
-    print(downloaded)
 
+    # Download only if they are not downloaded yet.
+    downloaded = downloader.download_all(iit_docs)
+
+    schedules = None  # type: ScheduleData | None
     for doc in downloaded:
-        schedule_data = ExcelScheduleParser(doc[1]).parse()
-        print(schedule_data.get_groups())
+        if schedules is None:
+            schedules = ExcelScheduleParser(doc[1]).parse()
+        else:
+            schedules.extend(ExcelScheduleParser(doc[1]).parse().get_schedule())
+
+    df = schedules.get_dataframe()
+    # get only room, room_type and campus from dataframe
+    df = df[["room", "room_type", "campus"]]
+    # get unique values
+    df = df.drop_duplicates()
+    # sort by room name
+    df = df.sort_values(by="room")
+    # get current script dir
+    current_dir = os.path.dirname(os.path.realpath(__file__))
+    # save dataframe to csv
+    df.to_csv(os.path.join(current_dir, "rooms.csv"))
