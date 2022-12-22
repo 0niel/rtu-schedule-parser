31bf46219e3800c87fadb915995ec870eefaafe7
Sergey Dmitriev <51058739+0niel@users.noreply.github.com>
2022-12-22 13:20:05 +0300

    refactor: Update package version

--- rtu_schedule_parser/__init__.py
+++ rtu_schedule_parser/__init__.py
@@ -1,6 +1,6 @@
-__version__ = "0.2.1"
+__version__ = "2.0.0"
 __author__ = "Sergey Dmitriev"
 
 from .excel_parser import ExcelScheduleParser
-from .schedule import ExamsSchedule, Lesson, LessonEmpty, LessonsSchedule
+from .schedule import ExamsSchedule,Exam, ExamEmpty, Lesson, LessonEmpty, LessonsSchedule
 from .schedule_data import ScheduleData
